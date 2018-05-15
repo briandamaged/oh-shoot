d05d4325664b0ffb2436b4de551a4f9cee09d1fd
Brian Lauber <blauber@arclia.com>
2018-05-15 00:20:40 -0400

    Ship can move around

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin;
+	public float xMax;
+
+	public float zMin;
+	public float zMax;
+
+
+	public float ClampX(float x) {
+		return Mathf.Clamp (x, this.xMin, this.xMax);
+	}
+
+	public float ClampZ(float z) {
+		return Mathf.Clamp (z, this.zMin, this.zMax);
+	}
+
+	public Vector3 Constrain(Vector3 pos) {
+		return new Vector3 (
+			this.ClampX(pos.x),
+			pos.y,
+			this.ClampZ(pos.z)
+		);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	public float speed = 2.0f;
+	public float tilt = 45.0f;
+
+	public Boundary boundary;
+
+
+	// Use this for initialization
+	void Start () {
+		this.rb = this.GetComponent<Rigidbody> ();
+	}
+
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		this.rb.velocity = this.speed * movement;
+
+		this.rb.position = this.boundary.Constrain (this.rb.position);
+
+		this.rb.rotation = Quaternion.Euler(0, 0, -moveHorizontal * this.tilt);
+	}
+}
