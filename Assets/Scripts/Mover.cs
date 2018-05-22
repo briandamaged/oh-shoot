ac017640b0aba0e769ae133764c23279dac22e3a
Brian Lauber <blauber@arclia.com>
2018-05-22 00:03:41 -0400

    Added shots

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		this.rb = this.GetComponent<Rigidbody> ();
+
+		this.rb.velocity = this.speed * this.transform.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,8 +9,14 @@ public class PlayerController : MonoBehaviour {
 	public float speed = 2.0f;
 	public float tilt = 45.0f;
 
+	public float fireRate = 0.5f;
+	private float nextFire = 0.0f;
+
 	public Boundary boundary;
 
+
+	public GameObject shot;
+	public Transform shotSpawn;
 
 	// Use this for initialization
 	void Start () {
@@ -28,5 +34,14 @@ public class PlayerController : MonoBehaviour {
 		this.rb.position = this.boundary.Constrain (this.rb.position);
 
 		this.rb.rotation = Quaternion.Euler(0, 0, -moveHorizontal * this.tilt);
+	}
+
+	void Update() {
+		float t = Time.time;
+		if(t > this.nextFire && Input.GetButton("Fire1")) {
+			this.nextFire = t + this.fireRate;
+			Instantiate<GameObject> (this.shot, this.shotSpawn.position, this.shotSpawn.rotation);
+		}
+
 	}
 }
