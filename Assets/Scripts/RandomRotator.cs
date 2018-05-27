745feb10956237c249bff6aec1b8014b24e88b5b
Brian Lauber <blauber@arclia.com>
2018-05-27 19:47:00 -0400

    Asteroids can rotate and be destroyed

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble = 1.0f;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		this.rb = this.GetComponent<Rigidbody> ();
+
+		this.rb.angularVelocity = Random.insideUnitSphere * this.tumble;
+	}
+
+}
