bf78f65d5c61fb16f144cdba33418f3e2e11101f
Brian Lauber <blauber@arclia.com>
2018-05-27 19:55:05 -0400

    Explosions!

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject explosion;
+
+	public GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other) {
 		if(other.tag == "Boundary") {
 			return;
 		}
+
+		Instantiate (this.explosion, this.transform.position, this.transform.rotation);
+
+		if(other.tag == "Player") {
+			Instantiate (this.playerExplosion, other.transform.position, other.transform.rotation);
+		}
+
 		Destroy (other.gameObject);
 		Destroy (this.gameObject);
+
 	}
 }
