7f26596062acfbfeb083cf6929c7f8fbd6fd9e20
Brian Lauber <blauber@arclia.com>
2018-05-27 20:02:52 -0400

    Random sizes for asteroids

---
+++ Assets/Scripts/RandomSizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSizer : MonoBehaviour {
+
+	public float minScale = 0.5f;
+	public float maxScale = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		float s = Random.Range (minScale, maxScale);
+
+		this.transform.localScale = new Vector3 (s, s, s);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
